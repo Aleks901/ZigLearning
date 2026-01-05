030e9954268fd164220141723f187ef2877a4f67
Aleks901 <aleks-j29@hotmail.com>
2026-01-06 00:34:35 +0100

    Tests and optimization
    - Optimized structs (None of the values ever need to go < 0. Besides ID I also just decided that none of the stats should ever go > 255. Just being nitpicky for the heck of it.
    - Added test for function

--- src/entities.zig
+++ src/entities.zig
@@ -2,20 +2,32 @@
 const std = @import("std");
 
 pub const Player = struct {
-    id: i32,
+    id: u32,
     name: []const u8,
-    health: i32,
-    mana: i32
+    health: u8,
+    mana: u8,
 };
 
 pub const Enemy = struct {
-    id: i32,
+    id: u32,
     name: []const u8,
-    health: i32,
-    mana: i32
+    health: u8,
+    mana: u8
 };
 
 pub fn attackTarget(target: *Enemy) !void
 {
     target.health -= 1;
+}
+
+test "Attack function reduces enemy health" {
+    var enemy = Enemy
+    {
+        .health = 10,
+        .id = 1,
+        .mana = 10,
+        .name = "TestEnemy"
+    };
+    try attackTarget(&enemy);
+    try std.testing.expectEqual(9, enemy.health);
 }
